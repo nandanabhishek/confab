ef7a296d56fdfe55a3bdc5ccce2dc7adaaf484de
nandanabhishek <abhisheknandan00@gmail.com>
2022-07-06 02:02:07 +0530

    updated static_root

--- confab/settings.py
+++ confab/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
